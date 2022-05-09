f6768e933d4c625c76e2bb1a0b0da980c342d116
DanteDPagano <dante.pagano@ies9012.edu.ar>
2022-05-09 17:37:43 -0300

    Creación
    Se crea la aplicación Blazor de TareasPendientes.

---
+++ Data/TareaItem.cs
@@ -0,0 +1,8 @@
+namespace Ejercicio_1_ListaDeTareas.Data
+{
+    public class TareaItem
+    {
+    public string? Titulo { get; set; }
+    public bool Finalizada { get; set; }        
+    }
+}
---
+++ Data/WeatherForecast.cs
@@ -0,0 +1,12 @@
+namespace Ejercicio_1_ListaDeTareas.Data;
+
+public class WeatherForecast
+{
+    public DateTime Date { get; set; }
+
+    public int TemperatureC { get; set; }
+
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
+
+    public string? Summary { get; set; }
+}
